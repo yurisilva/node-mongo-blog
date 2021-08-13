9c64afac1b3cb1df5721cb098038f49164478281
yurisilva <yurinascimentofarias@gmail.com>
2021-08-13 11:28:40 -0300

    Adding username, date and post content

--- database/models/Post.js
+++ database/models/Post.js
@@ -4,6 +4,11 @@ const PostSchema = new mongoose.Schema({
     title: String,
     description: String,
     content: String, 
+    username: String,
+    createdAt: { 
+        type: Date,
+        default: new Date()
+    }
 })
 
 const Post =  mongoose.model('Post', PostSchema)
